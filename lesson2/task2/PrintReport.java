bb6a2fe19356c3a39a42955a02cb7603fe5af7b9
badmitry <dmitrij88@bk.ru>
2020-05-18 13:31:14 +0300

    задание выполнено насколько я его понял

--- lesson2/task2/PrintReport.java
+++ lesson2/task2/PrintReport.java
@@ -1,9 +1,21 @@
 import java.util.*;
 class PrintReport{
-    public void output(List<ReportItem> items){
+    public static void outputToPrinter(List<ReportItem> items){
         System.out.println("Output to printer");
         for(ReportItem item : items){
             System.out.format("printer %s - %f \n\r", item.getDescription(), item.getAmount());
+        }
+    }
+	public static void outputToSkreen(List<ReportItem> items){
+        System.out.println("Output to Skreen");
+        for(ReportItem item : items){
+            System.out.format("show %s - %f \n\r", item.getDescription(), item.getAmount());
+        }
+    }
+    public static void outputToFile(List<ReportItem> items){
+        System.out.println("Output to File");
+        for(ReportItem item : items){
+            System.out.format("save %s - %f \n\r", item.getDescription(), item.getAmount());
         }
     }
 }
